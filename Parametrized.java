16c23d1fcf473bb6cc623266f48b71299d3289e6
Muhammad Adnan <131403461+Addii61@users.noreply.github.com>
2023-10-15 03:19:10 +0500

    Add files via upload

---
+++ Parametrized.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package parametrized.constructor;
+
+/**
+ *
+ * @author cui
+ */
+public class Parametrized {
+    /*private int a;
+    private String b;*/
+    public Parametrized(int a,String b,boolean c){
+        System.out.println(a+"\n"+b+"\n"+c);
+    }
+    
+}
---
+++ ParametrizedConstructor.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package parametrized.constructor;
+
+/**
+ *
+ * @author cui
+ */
+public class ParametrizedConstructor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Parametrized p=new Parametrized(12,"Ali",true);
+    }
+    
+}
